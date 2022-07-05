281c1df8cef7e6e87d4e54274cf82b7339d367d5
April Granzow <apriljgranzow@gmail.com>
2022-07-05 13:20:19 -0700

    Initial commit

---
+++ Elixir/day1.exs
@@ -0,0 +1,4 @@
+# Express some geometry objects using tuples: a two-dimensional point, a line, a circle, a polygon, and a triangle.
+# Write a function to compute the hypotenuse of a right triangle given the length of two sides.
+# Convert a string to an atom.
+# Test to see if an expression is an atom.
